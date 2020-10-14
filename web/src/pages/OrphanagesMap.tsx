f11a4e00cd14c64cc5853bcf5ef5ca20c54de29f
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-10-14 12:48:16 -0300

    fix: fix page name and her route

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg'
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
 
 function OrphanegesMap() {
   return (
@@ -26,12 +36,26 @@ function OrphanegesMap() {
       </aside>
 
       <Map
-        center={[-21.1341923,-47.9933518]}
+        center={[-21.1385554,-47.989704]}
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+        <Marker
+          icon={mapIcon}
+          position={[-21.133548, -47.986538]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Lar das meninas
+            <Link to="">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import OrphanegesMap from './pages/OrphanegesMap';
+import OrphanegesMap from './pages/OrphanagesMap';
 
 function Routes() {
   return (
