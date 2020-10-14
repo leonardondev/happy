2b8b9ed3bae989f1019132e8558910545951fb4d
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2020-10-13 22:52:41 -0300

    chore: Yup validation

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import orphanagesView from '../views/orphanages_view'
+import orphanagesView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
-import Orphanage from '../models/Orphanage'
+import Orphanage from '../models/Orphanage';
 
 
 export default {
@@ -39,11 +40,12 @@ export default {
     const orphanagesRepository = getRepository(Orphanage);
 
     const requestImages = request.files as Express.Multer.File[];
+
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -52,7 +54,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
